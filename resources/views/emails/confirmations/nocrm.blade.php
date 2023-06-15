8ac40e283120f2757a26d9f4ec3ce43abc1a2cc3
lucky1603 <sinisa.ristic@gmail.com>
2023-06-15 14:29:02 +0200

    Promenjena poruka u slucaju da ne prodje proveru CRM-a.

--- resources/views/emails/confirmations/nocrm.blade.php
+++ resources/views/emails/confirmations/nocrm.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 <p>Poštovani,</p>
-<p>Proverom je utvrđeno da se ne nalazite u našoj bazi nastavnika. Vaš nalog je kreiran bla, bla, bla...</p>
+<p>Proverom je utvrđeno da se ne nalazite u našoj bazi nastavnika. Vaš nalog je kreiran itd, itd, itd ...</p>
 @endsection
