fdfe5dd9b943be4120d1d2594573d80f219701f4
lucky1603 <sinisa.ristic@gmail.com>
2023-11-26 23:46:08 +0100

    Sredjen tekst email-a kada korisnika nema u CRM-u.

--- resources/views/emails/confirmations/nocrm.blade.php
+++ resources/views/emails/confirmations/nocrm.blade.php
@@ -2,5 +2,10 @@
 
 @section('content')
 <p>Poštovani,</p>
-<p>Proverom je utvrđeno da se ne nalazite u našoj bazi nastavnika. Vaš nalog je kreiran itd, itd, itd ...</p>
+<p>Proces potvrde korisničkog naloga kojim ste se registrovali je u toku. Uskoro ćemo Vas obavestiti o statusu Vašeg korisničkog naloga.</p>
+<p>Ovo je automatski generisana poruka, molimo Vas nemojte da odgovarate na nju. U slučaju da imate dodatna pitanja, pišite nam na
+    <a href="mailto:podrska@klett.rs" target="_blank">podrska@klett.rs</a>.</p>
+<p>Srdačan pozdrav,<br/>
+    Grupa Klett Srbija
+</p>
 @endsection
