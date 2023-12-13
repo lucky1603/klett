f2624649a1dac287bbf6a5fc3a46eef56eca9366
Sinisa Ristic <sinisa.ristic@gmail.com>
2023-12-13 23:53:46 +0100

    Popravljen template za registracionu formu.

--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -19,26 +19,19 @@
     <!-- Localization -->
     <script src="/lang-{{ app()->getLocale() }}.js"></script>
 </head>
-<body class="applyform">
+<body class="applyform w-100">
     <div id="app">
         <!-- Header with logos -->
-        {{-- <div class="container-fluid d-flex align-items-center justify-content-center bg-warning" style="height: 200px">
-            <img src="/images/klett1.png" style="height: 60px; margin: 20px"/>
-            <img src="/images/logos.png" style="height: 60px; margin: 20px"/>
-            <img src="/images/freska.png" style="height: 60px; margin: 20px"/>
-            <img src="/images/eucilogo1.png" style="height: 60px; margin: 20px"/>
-            <img src="/images/eucionica.png" style="height: 60px; margin: 20px"/>
-            <img src="/images/eknjizaralogo.svg" style="height: 60px; margin: 20px"/>
-        </div> --}}
-        <div class="shadow bg-light mx-4" style="width: 98%">
+        <div class="shadow bg-light" style="width: 100%">
             <div class="container-fluid px-0">
-                <div class="row w-100 mx-0" style="height: 100px; margin: auto 0;">
-                    <div class="col">
-                        <img src="/images/Header_KLF-logotipi.svg" width="500" class="float-right" style="margin-top: 20px"/>
+                <div class="row w-100 mx-0">
+                    <div class="col-12 offset-sm-2 col-sm-4">
+                        <img src="/images/Header_KLF-logotipi.svg" class="float-xl-right float-center my-4" style="width: 100%;"/>
                     </div>
-                    <div class="col" style="background-color: #efefef">
-                        <img src="/images/Header_EEE-logotipi.svg" width="500" class="float-left" style="margin-top: 20px"/>
+                    <div class="col-12 col-sm-4" style="background-color: #efefef">
+                        <img src="/images/Header_EEE-logotipi.svg" class="float-xl-left float-center my-4" style="width: 100%; "/>
                     </div>
+                    <div class="col-sm-2"></div>
                 </div>
             </div>
 
