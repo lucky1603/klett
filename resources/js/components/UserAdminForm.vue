6de7a52e8fa6aed5ea56ddae5aaf3e1245c68565
Sinisa Ristic <sinisa.ristic@gmail.com>
2024-08-20 13:48:10 +0200

    Omoguciti promenu klf sajtovi i  za administratore platforme.

--- resources/js/components/UserAdminForm.vue
+++ resources/js/components/UserAdminForm.vue
@@ -123,7 +123,7 @@
             <hr />
             <div v-if="!anonimous">
                 <b-form-checkbox v-model="form.klf_korisnik" :value="true" :disabled="!superAdmin">KLF Korisnik</b-form-checkbox>
-                <b-form-checkbox v-model="form.klf_sajtovi" :value="true" :disabled="!superAdmin">KLF Sajtovi</b-form-checkbox>
+                <b-form-checkbox v-model="form.klf_sajtovi" :value="true">KLF Sajtovi</b-form-checkbox>
                 <b-form-checkbox v-model="form.pedagoska_sveska" :value="true">Pedagoška sveska</b-form-checkbox>
                 <b-form-checkbox v-model="form.testomat" :value="true">Testomat</b-form-checkbox>
                 <b-form-checkbox v-model="form.enabled" :value="true">{{ _('gui.enabled') }}</b-form-checkbox>
