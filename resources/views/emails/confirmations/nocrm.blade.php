fc88f76121a72ca2b74450040fef77d98147cb30
Sinisa Ristic <sinisa.ristic@gmail.com>
2024-09-19 16:03:06 +0200

    Popravljen mail konfirmacije prilikom registracije kada korisnika nema u CRM-u.

--- resources/views/emails/confirmations/nocrm.blade.php
+++ resources/views/emails/confirmations/nocrm.blade.php
@@ -4,7 +4,7 @@
 <p>Poštovani,</p>
 <p>Proces potvrde korisničkog naloga kojim ste se registrovali je u toku. Uskoro ćemo Vas obavestiti o statusu Vašeg korisničkog naloga.</p>
 <p>Ovo je automatski generisana poruka, molimo Vas nemojte da odgovarate na nju. U slučaju da imate dodatna pitanja, pišite nam na
-    <a href="mailto:podrska@klett.rs" target="_blank">podrska@klett.rs</a>.</p>
+    <a href="mailto:tehnicka.podrska@klett.rs" target="_blank">tehnicka.podrska@klett.rs</a>.</p>
 <p>Srdačan pozdrav,<br/>
     Grupa Klett Srbija
 </p>
